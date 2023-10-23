b0e051fa27674084386a2ec6fd8a1080bf6824ad
SamMelgar <samsama.melgar@gmail.com>
2023-10-23 12:13:22 -0400

    created a table for array

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// Create a starting array of free lancers
-
-const p = document.querySelector("p");
+//Create initial array with prices
 const prices = [
     {name:"Alice", occupation:"Writer", startingPrice: 30},
     {name:"Bob", occupation:"Teacher", startingPrice: 50},
-    {name:"Carol", occupationg:"Programmer", startingPrice: 70}
+    {name:"Carol", occupation:"Programmer", startingPrice: 70}
 ];
-//Print array to document
-const freelancelistDiv = document.getElementById("freelancer-list")
 
+// Creates table for array on document
+const tableBody = document.querySelector("#freelancer-table tbody");
+
+prices.forEach(freelancer => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${freelancer.name}</td>
+        <td>${freelancer.occupation}</td>
+        <td>$${freelancer.startingPrice}</td>
+    `;
+    tableBody.appendChild(row);
+});
 //function to calculate average starting price
+const p = document.querySelector("p");
  p.textContent = "Average Freelancer starting price is"
