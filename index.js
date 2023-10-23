53804bd3f886fbd6c6d138af8e3576d7c55dfad5
SamMelgar <samsama.melgar@gmail.com>
2023-10-23 12:45:43 -0400

    completed, fixed errors that resulted in NaN error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const prices = [
 ];
 
 // Creates table for array on document
+
 const tableBody = document.querySelector("#freelancer-table tbody");
 
 prices.forEach(freelancer => {
@@ -17,6 +18,23 @@ prices.forEach(freelancer => {
     `;
     tableBody.appendChild(row);
 });
-//function to calculate average starting price
+
+//function to calculate Average from array
+
+function calculateAverage(prices) {
+    if (prices/length === 0) {
+        return 0;
+    }
+    const total = prices.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return total / prices.length;
+}
+
+//function to extra starting price to a separate array
+const startingPrice = prices.map(freelancer => freelancer.startingPrice);
+
+//calculate average starting price
+const averageStartingPrice = calculateAverage(averageStartingPrice);
+
+//Prints average price to document
 const p = document.querySelector("p");
- p.textContent = "Average Freelancer starting price is"
+ p.textContent = `Average Freelancer starting price is $${averageStartingPrice.toFixed(2)}`;
