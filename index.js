207cb264c9bb37e2cc2145af5152dfeb3c8ac494
SamMelgar <samsama.melgar@gmail.com>
2023-10-26 20:30:16 -0400

    need to improve calculated average

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-//Create initial array with prices
-const prices = [
-    {name:"Alice", occupation:"Writer", startingPrice: 30},
-    {name:"Bob", occupation:"Teacher", startingPrice: 50},
-    {name:"Carol", occupation:"Programmer", startingPrice: 70}
-];
-
-// Creates table for array on document
 
 const tableBody = document.querySelector("#freelancer-table tbody");
 
-prices.forEach(freelancer => {
+// generates a random Freelancer 
+function generateRandomFreelancer() {
+    const names = ["Pete", "Mark", "Susan", "Dorothy", "Kansas", "Angel"];
+    const occupations = ["Developer", "Tutor", "Consultant"];
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomOccupation = occupations[Math.floor(Math.random() * occupations.length)];
+    const randomStartingPrice = Math.floor(Math.random() * 100) + 30; // Generates random price between 30 and 129
+    return { name: randomName, occupation: randomOccupation, startingPrice: randomStartingPrice.toFixed(0) };
+}
+// renders freelancer to table
+function renderFreelancer(freelancer) {
     const row = document.createElement("tr");
     row.innerHTML = `
         <td>${freelancer.name}</td>
@@ -17,24 +19,31 @@ prices.forEach(freelancer => {
         <td>$${freelancer.startingPrice}</td>
     `;
     tableBody.appendChild(row);
+}
+//inital array given
+const prices = [
+    { name: "Alice", occupation: "Writer", startingPrice: 30 },
+    { name: "Bob", occupation: "Teacher", startingPrice: 50 },
+    { name: "Carol", occupation: "Programmer", startingPrice: 70 }
+];
+//renders prices
+prices.forEach(freelancer => {
+    renderFreelancer(freelancer);
 });
+// sets interval and pushes new freelancer
+const addRandomFreelancer = setInterval(function() {
+    const randomFreelancer = generateRandomFreelancer();
+    prices.push(randomFreelancer);
+    renderFreelancer(randomFreelancer);
+    updateAveragePrice();
+}, 1000); //every second
 
-//function to calculate Average from array
 
+//calculates average for prices
 function calculateAverage(prices) {
     if (prices.length === 0) {
         return 0;
     }
-    const total = prices.reduce((accumulator, currentValue) => accumulator + currentValue);
+    const total = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     return total / prices.length;
 }
-
-//function to extra starting price to a separate array
-const startingPrices = prices.map(freelancer => freelancer.startingPrice);
-
-//calculate average starting price
-const averageStartingPrice = calculateAverage(startingPrices);
-
-//Prints average price to document
-const p = document.querySelector("p");
- p.textContent = `Average Freelancer starting price is $${averageStartingPrice.toFixed(2)}`;
